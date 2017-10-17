5043ee71bd74ce98499bbc47cf03511a9f03f866
Surflyan <yanjiliang0128@outlook.com>
2017-10-17 22:59:05 +0800

    feat: add PageParser class
    add a PageParser class for target link, handle the HTML page with Jsoup.

--- module-scraper/src/main/java/com/codebelief/app/scraper/MyCrawler.java
+++ module-scraper/src/main/java/com/codebelief/app/scraper/MyCrawler.java
@@ -1,39 +1,39 @@
 package com.codebelief.app.scraper;
 
 import java.util.regex.Pattern;
+import com.codebelief.app.scraper.PageParser;
 
-import java.util.Set;
+import org.jsoup.select.Elements;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.WebCrawler;
 import edu.uci.ics.crawler4j.parser.HtmlParseData;
 import edu.uci.ics.crawler4j.url.WebURL;
 
+/**
+ * @author Surflyan
+ */
+
 public class MyCrawler extends WebCrawler {
     private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|gif|jpg"
                                                            + "|png|mp3|mp3|zip|gz))$");
- 
-    /**
-     * This method receives two parameters. The first parameter is the page
-     * in which we have discovered this new url and the second parameter is
-     * the new url. You should implement this function to specify whether
-     * the given url should be crawled or not (based on your crawling logic).
-     * In this example, we are instructing the crawler to ignore urls that
-     * have css, js, git, ... extensions and to only accept urls that start
-     * with "http://www.ics.uci.edu/". In this case, we didn't need the
-     * referringPage parameter to make the decision.
-     */
+     
+     /*
+      * 定义爬虫开始网页，以及接下来爬取网页的规则
+      * 本爬虫系统，只下载特定页面，无需爬取更深层次页面
+      * 始终返回（false）,留作功能拓展使用
+      */
      @Override
      public boolean shouldVisit(Page referringPage, WebURL url) {
          String href = url.getURL().toLowerCase();
-//         return !FILTERS.matcher(href).matches()
-//                && href.startsWith("http://www.ics.uci.edu/");
-         return false;
+         return !FILTERS.matcher(href).matches()
+                && href.startsWith("http://www.ics.uci.edu/");
      }
  
-     /**
-      * This function is called when a page is fetched and ready
-      * to be processed by your program.
+     /*
+      * 处理爬取后的页面，提取有效链接（指向文章的链接）
       */
      @Override
      public void visit(Page page) {
@@ -42,13 +42,28 @@ public class MyCrawler extends WebCrawler {
  
          if (page.getParseData() instanceof HtmlParseData) {
              HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
-             String text = htmlParseData.getText();
              String html = htmlParseData.getHtml();
-             Set<WebURL> links = htmlParseData.getOutgoingUrls();
- 
-             System.out.println("Text length: " + text.length());
-             System.out.println("Html length: " + html.length());
-             System.out.println("Number of outgoing links: " + links.size());
+             String title = htmlParseData.getTitle();
+             
+             System.out.println("Title: "+ title);           
+             String baseUri = url;
+             Elements validLinks = PageParser.getLinks(html, baseUri);
+             writeFile(html);
+             
          }
-    }
+     }
+    
+     /*
+      * 将爬取的 html 页面写入文件（临时测试）
+      */
+     public void writeFile(String html) {
+    	 try {
+	    	 FileWriter fw = new FileWriter("html.txt");
+	    	 fw.write(html);
+	    	 fw.flush();
+	    	 fw.close();
+    	 }catch (IOException e) {
+    		 e.printStackTrace();
+    	}
+     }
 }
