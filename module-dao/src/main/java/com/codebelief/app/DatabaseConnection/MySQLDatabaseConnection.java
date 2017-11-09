3c2f248630861d6c94ca0369c555eebd7f4902c7
hetao <15705657659@163.com>
2017-11-10 02:05:40 +0800

    second version

--- module-dao/src/main/java/com/codebelief/app/DatabaseConnection/MySQLDatabaseConnection.java
+++ module-dao/src/main/java/com/codebelief/app/DatabaseConnection/MySQLDatabaseConnection.java
@@ -1,9 +1,7 @@
 package com.codebelief.app.DatabaseConnection;
 
 import java.util.*;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -17,30 +15,25 @@ import java.sql.SQLException;
  */
 public class MySQLDatabaseConnection implements DatabaseConnection{
 	
-	private String DriverClass = null;//"com.mysql.jdbc.Driver";
-	private String Url = null;//"jdbc:mysql://localhost:3306/bookdb";
-	private String UserName = null;//"root";
-	private String Password = null;//"www.12358.com";
+	private static String host = null;
+	private static String port = null;
+	private static String database = null;
+	private static String userName = null;
+	private static String password = null;
 	private Connection conn = null;
+	private String url = null;
 	
-	public MySQLDatabaseConnection() throws IOException{
-		Properties prop = new Properties();
-		FileInputStream file = new FileInputStream("F://java/workspace/SeeUTomorrow/src/dbProperties.properties");
-		prop.load(file);
-		
-		DriverClass = prop.getProperty("DriverClass");
-		Url = prop.getProperty("Url");
-		UserName = prop.getProperty("UserName");
-		Password = prop.getProperty("Password");
+	public MySQLDatabaseConnection() throws IOException {
+		url="jdbc:mysql://"+host+":"+port+"/"+database+"?characterEncoding=utf-8";
 		
 		try {
-			Class.forName(DriverClass);
+			Class.forName("com.mysql.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 		
 		try {
-			conn = DriverManager.getConnection(Url,UserName,Password);
+			conn = DriverManager.getConnection(url,userName,password);
 		} catch (SQLException e) {
 			System.out.print("Connecting Action failed!");
 			e.printStackTrace();
@@ -48,11 +41,20 @@ public class MySQLDatabaseConnection implements DatabaseConnection{
 	}
 	
 	@Override
+	/**
+	 * @Title: getConnection
+	 * @Description: return the Connection to the database
+	 * @return Connection
+	 */
 	public Connection getConnection() {
 		return conn;
 	}
 
 	@Override
+	/**
+	 * @Title: free
+	 * @Description: Close the connection to the database
+	 */
 	public void free() {
 		try {
 			conn.close();
@@ -61,5 +63,103 @@ public class MySQLDatabaseConnection implements DatabaseConnection{
 			e.printStackTrace();
 		}
 	}
+
+	/**
+	 * @Title: initialDatabaseDeploy
+	 * @Description: Initiate the database configuration.
+	 * 				 If the needed configuration file, 
+	 * 				 db.properties, is not exist under the path of ${user.home}/.wus/db.properties,
+	 * 				 then build it under this path.
+	 * 				 If the contents contained in needed configuration file are not complete,
+	 * 				 then add the default settings into the configuration file.
+	 * 				 Else use the file that developers has builded directly.
+	 */
+	public static void initialDatabaseDeploy() {
+		String home = System.getProperty("user.home");
+		String fileSeparator = System.getProperty("file.separator");
+		String path = home + fileSeparator+".wus"+fileSeparator+"db.properties";
+		File file = new File(path);
+		if(!file.exists()){
+			try {
+				createDefaultDbProperties(path,"root","musql","localhost","3306","musql");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		else{
+			try {
+				readDbProperties(path);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	/**
+	 * 
+	 * @Title: createDefaultDbProperties
+	 * @Description: Create the configuration file if this file is not exist
+	 * 				 using the given arguments.
+	 * @param path
+	 * @param username
+	 * @param password
+	 * @param host
+	 * @param port
+	 * @param database
+	 * @throws IOException
+	 */
+	private static void createDefaultDbProperties(String path, 
+			String username, String password, String host, String port, String database) throws IOException{
+		File file = new File(path);
+		if(!file.getParentFile().exists()){
+			file.getParentFile().mkdirs();
+		}
+		file.createNewFile();
+		
+		FileWriter fw = new FileWriter(file);
+		BufferedWriter write = new BufferedWriter(fw);
+		write.write("username="+username);
+		write.newLine();
+		write.write("password="+password);
+		write.newLine();
+		write.write("host="+host);
+		write.newLine();
+		write.write("database="+database);
+		write.newLine();
+		write.write("port="+port);
+		write.flush();
+		
+		write.close();
+		fw.close();
+	}
 	
+	/**
+	 * 
+	 * @Title: readDbProperties
+	 * @Description: Get the configuration information from /.wus/do.properties
+	 * @param path
+	 * @throws IOException
+	 */
+	private static void readDbProperties(String path) throws IOException{
+		Properties prop = new Properties();
+		FileInputStream file = null;
+		try {
+			file = new FileInputStream(path);
+		} catch (FileNotFoundException e1) {
+			
+			e1.printStackTrace();
+		}
+		try {
+			prop.load(file);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		userName = prop.getProperty("username");//"root";
+		password = prop.getProperty("password");//"mysql";
+		host = prop.getProperty("host");//"localhost";
+		port = prop.getProperty("port");//"3306";
+		database = prop.getProperty("database");//"mysql";
+	}
 }
--- module-dao/src/main/java/com/codebelief/app/test/DAOTest.java
+++ module-dao/src/main/java/com/codebelief/app/test/DAOTest.java
@@ -2,7 +2,10 @@ package com.codebelief.app.test;
 
 import com.codebelief.app.DAO.*;
 import com.codebelief.app.DAOFactory.*;
+import com.codebelief.app.DatabaseConnection.DatabaseConnectionFactory;
+import com.codebelief.app.DatabaseConnection.MySQLDatabaseConnection;
 import com.codebelief.app.VO.*;
+import com.codebelief.app.DAOFactory.*;
 
 import java.sql.Time;
 /**
@@ -11,27 +14,29 @@ import java.sql.Time;
  */
 public class DAOTest {
 	public static void main(String[] args) throws Exception{
-		User user=new User("何涛","hetao","15705657659@163.com",Time.valueOf("20:00:00"));
+		MySQLDatabaseConnection.initialDatabaseDeploy();
+		MySQLDatabaseConnection mysql = DatabaseConnectionFactory.getMySQLDatabaseConnection();
+		//User user=new User("何涛","hetao","15705657659@163.com",Time.valueOf("20:00:00"));
 		IUserDAO userDAO=UserDAOFactory.getUserDAOInstance();
 		//userDAO.doInsert(user);
-		userDAO.doUpdatePushTime("何涛", Time.valueOf("20:00:00"));
+		userDAO.doUpdatePushTime("何涛", Time.valueOf("21:00:00"));
 		//userDAO.doUpdatePasswordAndEmail("何涛", "hetao", "15705657659@163.com");
 		/*User newUser=userDAO.doFindAll("何涛");
 		System.out.println("UserName:"+newUser.getUserName());
 		System.out.println("Password:"+newUser.getPassword());
 		System.out.println("Email:"+newUser.getEmail());
 		System.out.println("PushTime:"+newUser.getPushTime());*/
-		String Password=userDAO.doFindPassword("何涛");
-		System.out.println("Passowrd:"+Password);
+		/*String password=userDAO.doFindPassword("何涛");
+		System.out.println("Passowrd:"+password);
 		String Email=userDAO.doFindEmail("何涛");
 		System.out.println("Email:"+Email);
 		Time PushTime=userDAO.doFindPushTime("何涛");
 		System.out.println("PushTime:"+PushTime);
-		userDAO.free();
+		userDAO.free();*/
 		
-		Url url=new Url(10010,"何涛","question","www.Github.com");
-		IUrlDAO urlDAO=UrlDAOFactory.getUrlDAOInstance();
-		urlDAO.doDelete(10010);
+		//Url url=new Url(10010,"何涛","question","www.Github.com");
+		//IUrlDAO urlDAO=UrlDAOFactory.getUrlDAOInstance();
+		//urlDAO.doDelete(10010);
 		//urlDAO.doInsert(url);
 		/*Url newUrl=urlDAO.doFindAll(10010);
 		System.out.println(newUrl.getUrl());
@@ -45,7 +50,7 @@ public class DAOTest {
 		System.out.println(""+urlDAO.doFindRealTimePush(10010));*/
 		//urlDAO.doUpdate(10010, "www.stackOverflow.com", false, false);
 		//urlDAO.doDelete(10010);
-		urlDAO.free();
+		//urlDAO.free();
 		
 		//Content content = new Content(1001,10010,"<html><head></head></html>","<head></head>");
 		//IContentDAO contentDAO=ContentDAOFactory.getContentDAOInstance();
@@ -62,6 +67,10 @@ public class DAOTest {
 		//contentDAO.doUpdateHtmlAndDelta(1001, "<html><body></body></html>", "<body></body>");
 		//contentDAO.doDelete(1001);
 		//contentDAO.Free();
+		//MySQLDatabaseConnection dbc = DatabaseConnectionFactory.getMySQLDatabaseConnection();
+		//MySQLDatabaseConnection.initialDatabaseDeploy();
+		//MySQLDatabaseConnection mysql = DatabaseConnectionFactory.getMySQLDatabaseConnection();
+		//System.out.println(System.getProperties().getProperty("file.separator"));
 		System.out.println("Success!");
 	}
 }
